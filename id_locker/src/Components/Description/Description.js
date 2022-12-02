c32e00b00d70951c4272de86dfaa5a7b36592d15
Filip Ruminski <fruminski@gmail.com>
2022-12-02 15:52:43 +0000

    fixed typo

--- id_locker/src/Components/Description/Description.js
+++ id_locker/src/Components/Description/Description.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Description.css'
 
 const Description = () => {
-    return <p>Store all your important information securly in one place</p>
+    return <p>Store all your important information securely in one place</p>
 }
 
 export default Description
